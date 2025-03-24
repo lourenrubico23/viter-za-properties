7c94aba4686002030ee89540642538ad79554ad0
Louren Rubico <louren.rubico@frontlinebusiness.com.ph>
2025-03-24 13:49:48 +0800

    buttons in properties and blogs updated

--- src/components/partials/BlogList.jsx
+++ src/components/partials/BlogList.jsx
@@ -123,7 +123,10 @@ const BlogList = ({ pageType, currentBlogSlug }) => {
           <div className="place-self-center">
             {hasMoreProperties ? (
               <button
-                className="text-sm font-semibold"
+                className="text-sm font-semibold relative pb-1 transition duration-300 
+                before:absolute before:bottom-0 before:left-0 before:w-full before:h-0.5 
+                before:bg-secondary before:transition-transform before:duration-300 before:scale-x-0 
+                hover:before:scale-x-100"
                 onClick={handleLoadMore}
               >
                 Load more blogs
